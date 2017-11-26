a55f8174ee55504b03e8520589f9a159c8219fc3
Kitson Kelly <me@kitsonkelly.com>
2017-11-26 15:03:08 +0000

    Fix icon name in manifest.json

--- src/themes/devtool/index.ts
+++ src/themes/devtool/index.ts
@@ -1,12 +1,16 @@
 import { assign } from '@dojo/shim/object';
 import dojoTheme from '@dojo/widgets/themes/dojo/theme';
 
+import * as actionbar from './actionbar.m.css';
+import * as actionbarbutton from './actionbarbutton.m.css';
 import * as devtool from './devtool.m.css';
 import * as itemlist from './itemlist.m.css';
 import * as treepane from './treepane.m.css';
 import * as vdom from './vdom.m.css';
 
 const devtoolTheme = assign({}, dojoTheme, {
+	actionbar,
+	actionbarbutton,
 	devtool,
 	itemlist,
 	treepane,
